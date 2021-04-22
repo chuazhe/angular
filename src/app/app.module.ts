02f5aeccbc31ed296bf73ca950bf05a790ad2a49
Chua Wei Zhe <chuazhe@gmail.com>
2021-04-22 17:15:05 +0800

    Angular-0.0.0_6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { StocksService } from './services/stocks.service';
 import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [[StocksService]],
   bootstrap: [AppComponent],
 })
